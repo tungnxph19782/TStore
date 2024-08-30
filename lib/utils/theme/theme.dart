abc56b083b385b121194a22a4348594c60291f86
nguyentung <nguyenxuantung003@gmail.com>
2024-08-30 17:58:52 +0700

    update theme

--- lib/utils/theme/theme.dart
+++ lib/utils/theme/theme.dart
@@ -1,5 +1,12 @@
 import 'package:flutter/material.dart';
 import 'package:ecommerce_app/utils/theme/custom_themes/text_theme.dart';
+import 'custom_themes/appbar_theme.dart';
+import 'custom_themes/bottom_sheet_theme.dart';
+import 'custom_themes/checkbox_theme.dart';
+import 'custom_themes/chip_theme.dart';
+import 'custom_themes/elevated_button_theme.dart';
+import 'custom_themes/text_field_theme.dart';
+import 'custom_themes/text_theme.dart';
 
 class TAppTheme {
   TAppTheme._();
@@ -8,17 +15,28 @@ class TAppTheme {
     useMaterial3: true,
     brightness: Brightness.light,
     primaryColor: Colors.blue,
-    scaffoldBackgroundColor: Colors.white,
     textTheme: TTextTheme.lightTextTheme,
-    elevatedButtonTheme: ElevatedButtonThemeData()
-
+    chipTheme: TChipTheme.lightChipTheme,
+    scaffoldBackgroundColor: Colors.white,
+    appBarTheme: TAppbarTheme.lightAppBarTheme,
+    checkboxTheme: TCheckboxTheme.lightCheckBoxTheme,
+    bottomSheetTheme: TBottomSheetTheme.lightBottomSheetTheme,
+    elevatedButtonTheme: TElevatedButtonTheme.lightElevatedButtonTheme,
+    inputDecorationTheme: TTextFieldTheme.lightInputDecorationTheme,
   );
   static ThemeData darkTheme = ThemeData(
     useMaterial3: true,
     brightness: Brightness.dark,
     primaryColor: Colors.blue,
-    scaffoldBackgroundColor: Colors.black,
     textTheme: TTextTheme.darkTextTheme,
+    chipTheme: TChipTheme.drakChipTheme,
+    scaffoldBackgroundColor: Colors.black,
+    appBarTheme: TAppbarTheme.darkAppBarTheme,
+    checkboxTheme: TCheckboxTheme.darkCheckBoxTheme,
+    bottomSheetTheme: TBottomSheetTheme.darkBottomSheetTheme,
+    elevatedButtonTheme: TElevatedButtonTheme.darkElevatedButtonTheme,
+    inputDecorationTheme: TTextFieldTheme.darkInputDecorationTheme,
+
   );
 
 }
